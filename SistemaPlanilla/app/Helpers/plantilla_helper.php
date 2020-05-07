5b9b99f2f5739d56627abaad900275500ef780cf
Rodrigo Presa <rodrigopresamar@gmail.com>
2020-05-07 13:04:55 -0600

    sidebar parametrizable

---
+++ SistemaPlanilla/app/Helpers/plantilla_helper.php
@@ -0,0 +1,176 @@
+<?php
+
+date_default_timezone_set ('America/El_Salvador');
+setlocale(LC_TIME, 'es_SV');
+
+function crear_plantilla($main = ''){
+    return crear_head().crear_body($main);
+}
+
+function crear_head(
+    $titulo = 'Sin Backup'
+){
+    $datos = [
+        'titulo' => $titulo,
+    ];
+    return view('plantilla/head', $datos);
+}
+
+function crear_body(
+        $main = '',
+        $sidebar = '',
+        $breadcrumb = '',
+        $navbar = '',
+        $footer = ''
+    ){
+    $navbar = ($navbar == '') ? crear_navbar() : $navbar;
+    $breadcrumb = ($breadcrumb == '') ? crear_breadcrumb() : $breadcrumb;
+    $sidebar = ($sidebar == '') ? crear_sidebar() : $sidebar;
+    $main = ($main == '') ? '<h1>Sin Contenido</h1>' : $main;
+    $footer = ($footer == '') ? crear_footer() : $footer; 
+
+    $datos = [
+        'navbar'     => $navbar,
+        'breadcrumb' => $breadcrumb,
+        'sidebar'    => $sidebar,
+        'main'       => $main,
+        'footer'     => $footer,
+    ];
+    return view('plantilla/body', $datos);
+}
+
+function crear_navbar(
+    $enlaces = [],
+    $fecha = ''
+){
+    if($fecha == ''){
+        $fecha = date("d") . "/" . date("m") . "/" . date("Y") . "   ".date("H:i:s");  ;
+    }
+    if(count($enlaces) == 0){
+        $enlaces = [[
+            'nombre' => 'Casa',
+            'url' => base_url()
+        ]];
+    }
+    $datos = [
+        'enlaces' => $enlaces,
+        'fecha' => $fecha,
+    ];
+    return view('plantilla/navbar', $datos);
+}
+
+function crear_breadcrumb(
+    $titulo  = 'Dashboard',
+    $ruta = []
+){
+    if(count($ruta) == 0){
+        $ruta = [[
+                'nombre' => 'Dashboard',
+                'url' => base_url()
+            ],[
+                'nombre' => 'Casa',
+                'url' => base_url()
+            ]];
+    }
+    $datos = [
+        'titulo' => $titulo,
+        'ruta' => $ruta,
+    ];
+    return view('plantilla/breadcrumb', $datos);
+}
+
+function crear_sidebar(
+    $usuario = [],
+    $menus = []
+){
+    if(count($usuario) == 0){
+        $usuario = [
+            'nombre' => 'Dixon Argueta',
+            'img'    => 'user2-160x160.jpg',
+            'url'    => base_url()
+        ];
+    }
+    if(count($menus) == 0){
+        $menus = [
+            [
+                'nombre'   => 'Empresa',
+                'url'      => base_url(),
+                'icono'    => 'fas fa-building',
+                'submenus' => [
+                    [
+                        'nombre'   => 'Datos Empresa',
+                        'url'      => base_url()
+                    ],
+                    [
+                        'nombre'   => 'Divisiones',
+                        'url'      => base_url()
+                    ]
+                ]
+            ],
+            [
+                'nombre'   => 'Planilla',
+                'url'      => base_url(),
+                'icono'    => 'fas fa-copy',
+                'submenus' => [
+                    [
+                        'nombre'   => 'Boleta de pago',
+                        'url'      => base_url()
+                    ],
+                    [
+                        'nombre'   => 'ISSS, AFP',
+                        'url'      => base_url()
+                    ],
+                ]
+            ],
+            [
+                'nombre'   => 'Empleados',
+                'url'      => base_url(),
+                'icono'    => 'fas fa-address-card',
+                'submenus' => [
+                    [
+                        'nombre'   => 'Datos Empleados',
+                        'url'      => base_url()
+                    ],
+                    [
+                        'nombre'   => 'Nuevo Empleado',
+                        'url'      => base_url()
+                    ],
+                ]
+            ],
+            [
+                'nombre'   => 'Usuario',
+                'url'      => base_url(),
+                'icono'    => 'fas fa-users-cog',
+                'submenus' => [
+                    [
+                        'nombre'   => 'Roles Y permisos',
+                        'url'      => base_url()
+                    ],
+                    [
+                        'nombre'   => 'Activar y desactivar',
+                        'url'      => base_url()
+                    ],
+                ]
+            ]
+            ];
+    }
+    $datos = [
+        'usuario' => $usuario,
+        'menus'  => $menus
+    ];
+    return view('plantilla/sidebar', $datos);
+}
+
+function crear_footer(
+    $grupo = 'Sin Backup',
+    $materia = 'BAD115'
+
+){
+    $datos = [
+        'grupo' => $grupo,
+        'materia' => $materia
+        
+    ];
+    return view('plantilla/footer', $datos);
+}
+
---
+++ SistemaPlanilla/app/Views/plantilla/sidebar.php
@@ -0,0 +1,69 @@
+ <!-- Main Sidebar Container -->
+ <aside class="main-sidebar sidebar-dark-primary elevation-4">
+    <!-- Brand Logo -->
+    <a href="#" class="brand-link">
+      <img src="img/sin_backup.png" alt="AdminLTE Logo" class="brand-image img-circle "
+           style="opacity: .9" >
+      <span class="brand-text font-weight-bold">Sin Backup</span>
+    </a>
+
+    <!-- Sidebar -->
+    <div class="sidebar">
+      <!-- Sidebar user panel (optional) -->
+      <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+        <div class="image">
+          <img src="img/<?= $usuario['img']?>" class="img-circle elevation-2" alt="User Image">
+        </div>
+        <div class="info">
+          <a href="<?= $usuario['url']?>" class="d-block"><?= $usuario['nombre']?></a>
+        </div>
+      </div>
+
+      <!-- Sidebar Menu -->
+      <nav class="mt-2">
+        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+          <!-- Add icons to the links using the .nav-icon class
+               with font-awesome or any other icon font library -->
+         
+          <?php foreach ($menus as $menu): ?>
+          <li class="nav-item has-treeview">
+            <a href="<?=$menu['url']?>" class="nav-link">
+              <i class="nav-icon <?= $menu['icono']?>"></i>
+              <p>
+                <?= $menu['nombre']?>
+                <i class="fas fa-angle-left right"></i>
+              </p>
+            </a>
+            <ul class="nav nav-treeview">
+              <?php foreach ($menu['submenus'] as $submenu): ?>
+                <li class="nav-item">
+                  <a href="<?=$submenu['url'] ?>" class="nav-link">
+                    <i class="fas fa-arrow-alt-circle-right nav-icon"></i>
+                    <p><?= $submenu['nombre']?></p>
+                  </a>
+                </li>
+              <?php endforeach; ?>
+            </ul>
+          </li>
+          <?php endforeach; ?>
+
+          
+         
+
+          <li class="nav-header">Mas</li>
+          <li class="nav-item has-treeview">
+            <a href="#" class="nav-link">
+              <i class="nav-icon fas fa-sign-out-alt"></i>
+              <p>
+                Salir 
+              </p>
+            </a>
+           
+          </li>
+        
+        </ul>
+      </nav>
+      <!-- /.sidebar-menu -->
+    </div>
+    <!-- /.sidebar -->
+  </aside>
