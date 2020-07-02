bd7e76e526b815511b5b82a828e4a9179ca50bc6
dixon28 <33327336+dixon28@users.noreply.github.com>
2020-07-02 17:04:28 -0600

    se ha agregado codigo de la planilla
    formularios correspondientes

---
+++ SistemaPlanilla/app/Views/Boleta/resultado_import.php
@@ -0,0 +1,24 @@
+
+
+
+<div class="col-md-12">
+            <div class="card card-success">
+              <div class="card-header">
+                <h3 class="card-title">Ingrese un codigo</h3>
+
+                <div class="card-tools">
+                  <button type="button" class="btn btn-tool" data-card-widget="remove"><i class="fas fa-times"></i>
+                  </button>
+                </div>
+                <!-- /.card-tools -->
+              </div>
+              <!-- /.card-header -->
+              <div class="card-body">
+              <p>por favor ingrese un codigo valido</p>
+
+              </div>
+              <!-- /.card-body -->
+            </div>
+            <!-- /.card -->
+          </div>
+
